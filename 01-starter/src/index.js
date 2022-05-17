f3bb096d9e08d4036fb7a391fb8712f7ce9f3f75
Peter Onyegbule <49274497+webstellar@users.noreply.github.com>
2022-05-17 11:33:11 +0100

    Learning

--- 01-starter/src/index.js
+++ 01-starter/src/index.js
@@ -1,35 +1,76 @@
-import React from "react"
-import { createRoot } from "react-dom/client"
+import React, { useState } from "react";
+import { createRoot } from "react-dom/client";
 
 function App() {
+  const [size, setSize] = useState(15);
+  const [color, setColor] = useState("green");
+  const [likeCount, setLikeCount] = useState(0);
+
+  const ButtonHandler = () => {
+    setSize(20);
+    setColor("pink");
+  };
+
   return (
     <div className="grid-parent">
       <div className="header">
         <h1>Welcome To Our App</h1>
-        <p>The current size is x and the current color is x.</p>
         <p>
-          This page has been liked <strong>0</strong> times.
+          The current size is {size} and the current color is {color}.
+        </p>
+        <p>
+          This page has been liked <strong>{likeCount}</strong> times.
         </p>
       </div>
       <div className="sidebar">
-        <input type="text" />
-        <input type="text" />
-        <button>Make the text 20px and pink</button>
+        <input
+          type="text"
+          value={size}
+          onChange={(e) => {
+            setSize(e.target.value);
+          }}
+        />
+        <input
+          type="text"
+          value={color}
+          onChange={(e) => {
+            setColor(e.target.value);
+          }}
+        />
+        <button onClick={ButtonHandler}>Make the text 20px and pink</button>
       </div>
-      <div className="main-area">
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem, quod obcaecati dolore debitis amet aut, excepturi voluptas ratione quibusdam laboriosam, optio pariatur provident doloremque consequatur animi? Consequuntur, officiis. Explicabo, vel.</p>
+      <div className="main-area" style={{ color, fontSize: `${size}px` }}>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem, quod
+          obcaecati dolore debitis amet aut, excepturi voluptas ratione
+          quibusdam laboriosam, optio pariatur provident doloremque consequatur
+          animi? Consequuntur, officiis. Explicabo, vel.
+        </p>
       </div>
       <footer className="footer">
         <p>
-          This is the footer. <button>Make the text 30px but leave the color the same</button>
+          This is the footer.{" "}
+          <button
+            onClick={() => {
+              setSize(30);
+            }}
+          >
+            Make the text 30px but leave the color the same
+          </button>
         </p>
         <p>
-          <button>Like The Page</button>
+          <button
+            onClick={() => {
+              setLikeCount((prev) => prev + 1);
+            }}
+          >
+            Like The Page
+          </button>
         </p>
       </footer>
     </div>
-  )
+  );
 }
 
-const root = createRoot(document.querySelector("#app"))
-root.render(<App />)
+const root = createRoot(document.querySelector("#app"));
+root.render(<App />);
